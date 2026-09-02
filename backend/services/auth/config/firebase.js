4cd88093bb20b3d517bf34f40106bb8a6e525df5
Nishant Anand <nishant@gamil.com>
2026-09-02 16:44:40 +0530

    fix firebase admin configuration

--- backend/services/auth/config/firebase.js
+++ backend/services/auth/config/firebase.js
@@ -1,6 +1,16 @@
-import { cert, initializeApp } from "firebase-admin";
-import serviceAccount from "../serviceAccountKey.json"  with {type:"json"};
+// import { cert, initializeApp } from "firebase-admin";
+// import serviceAccount from "../serviceAccountKey.json"  with {type:"json"};
 
-export const app=initializeApp({
+// export const app=initializeApp({
+//   credential: cert(serviceAccount)
+// });
+
+import { cert, initializeApp } from "firebase-admin";
+
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT
+);
+
+export const app = initializeApp({
   credential: cert(serviceAccount)
 });
