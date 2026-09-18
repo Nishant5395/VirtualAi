b692a738a7a7c9e17cdb5aa130e2390d5287697d
Nishant Anand <nishant@gamil.com>
2026-09-18 23:26:59 +0530

    fix firebase error at local server

--- backend/services/auth/config/firebase.js
+++ backend/services/auth/config/firebase.js
@@ -1,16 +1,27 @@
-// import { cert, initializeApp } from "firebase-admin";
-// import serviceAccount from "../serviceAccountKey.json"  with {type:"json"};
+// // import { cert, initializeApp } from "firebase-admin";
+// // import serviceAccount from "../serviceAccountKey.json"  with {type:"json"};
 
-// export const app=initializeApp({
+// // export const app=initializeApp({
+// //   credential: cert(serviceAccount)
+// // });
+
+// import { cert, initializeApp } from "firebase-admin";
+
+// const serviceAccount = JSON.parse(
+//   process.env.FIREBASE_SERVICE_ACCOUNT
+// );
+
+// export const app = initializeApp({
 //   credential: cert(serviceAccount)
 // });
 
-import { cert, initializeApp } from "firebase-admin";
+import { cert, initializeApp } from "firebase-admin/app";
+import { readFileSync } from "fs";
 
 const serviceAccount = JSON.parse(
-  process.env.FIREBASE_SERVICE_ACCOUNT
+  readFileSync(new URL("../serviceAccountKey.json", import.meta.url), "utf8")
 );
 
 export const app = initializeApp({
-  credential: cert(serviceAccount)
+  credential: cert(serviceAccount),
 });
